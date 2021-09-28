9af7001442298c6e9c63870b22ed4c947d716835
Edy940 <edwandersilva@gmail.com>
2021-09-28 01:14:49 -0300

    chess layer and printing teh board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -30,4 +30,12 @@ public class Board {
         this.columns = columns;
     }
     
+    //retornando objeto
+    public Piece piece (int row, int column){
+        return pieces [row][column];
+    }
+    //Sobrecarga
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
 }
