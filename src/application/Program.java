a80044208687bd077d032b384f0d919a2e5b84e4
Edy940 <edwandersilva@gmail.com>
2021-09-27 23:19:38 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+/**
+ * @author Ed Wander
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        
+        System.out.println("HEllo word");
+
+    }
+
+}
